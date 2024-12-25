adbf3eb44ee657596db835c12d6010662db58d54
bi3mer <colan@comcast.net>
2024-12-25 12:14:27 -0600

    setting up game and imported stuff from Gram-Elites repo, but haven't folded it into everything else yet

---
+++ ponos/Game.py
@@ -0,0 +1,34 @@
+from typing import Dict, Any, List
+from random import seed
+from requests import get
+
+class Game:
+    def __init__(self, server: str, json_data: Dict[str, Any]):
+        self.completability_endpoint = f'{server}/completability'
+
+        self.elites_per_bin: int = json_data['elites-per-bin']
+        self.iterations: int = json_data['iterations']
+        self.max_strand_size: int = json_data['max-strand-size']
+        self.n: int = json_data['n']
+        self.start_population_size: int = json_data['start-population-size']
+        self.start_strand_size: int = json_data['start-strand-size']
+
+        if 'seed' in json_data:
+           seed(json_data['seed'])
+
+    def completability(self, level: List[str]) -> float:
+        c_request = get(self.completability_endpoint, params={'lvl': level})
+        if c_request.status_code != 200:
+            print(f"'{self.completability_endpoint}' endpoint returned status code: {c_request.status_code}")
+            exit(1)
+
+        try:
+            c = float(c_request.content)
+        except ValueError:
+            print(f"Completability response '{c_request.content}' could not be parsed to a float.")
+            exit(1)
+
+        assert c >= 0.0, "Completion value must be in the inclusive range of 0 and 1."
+        assert c <= 1.0, "Completion value must be in the inclusive range of 0 and 1."
+
+        return c
--- ponos/ponos.py
+++ ponos/ponos.py
@@ -1,3 +1,33 @@
 #!/usr/bin/env python
 
-print("hello, world")
+from Game import Game
+
+from requests import get
+import argparse
+import json
+
+def get_cmd_args() -> argparse.Namespace:
+    '''
+    Build arg parser used to get command line arguments from the user
+    '''
+    parser = argparse.ArgumentParser(description='Ponos')
+    parser.add_argument('--server', type=str, required=True, help="URL for game server.")
+
+    return parser.parse_args()
+
+def main():
+    args = get_cmd_args()
+
+    server = args.server
+    config_request = get(f'{server}/config')
+
+    if config_request.status_code != 200:
+        print(f"'{server}/config' endpoint returned status code: {config_request.status_code}")
+        exit(1)
+
+    G = Game(server, json.loads(config_request.content))
+    res = G.completability(["a", "b"])
+    print(res)
+
+if __name__ == '__main__':
+    main()
